57b11a94c2e1a5558387e81cf93eccb1aad307d2
Lily-Gold <lily.olive.yt@gmail.com>
2025-10-26 22:22:53 +0900

    test

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -1,6 +1,13 @@
 require "test_helper"
 
 class ReviewsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = users(:one)
+    sign_in @user
+  end
+
   test "should get index" do
     get reviews_url
     assert_response :success
