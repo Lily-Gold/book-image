ac508c354134c2ac933c865cb3d5298ef91f5f36
Lily-Gold <lily.olive.yt@gmail.com>
2025-11-01 21:49:45 +0900

    fix

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,9 +1,11 @@
-!/usr/bin/env bash
-# exit on error
+#!/usr/bin/env bash
 set -o errexit
 
+# 依存関係をインストール
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-# bundle exec rake db:migrate
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
+# CSS/JS のアセットを事前コンパイル
+bundle exec rails assets:precompile
+# 古いアセットを削除
+bundle exec rails assets:clean
+# DB マイグレーションを実行
+bundle exec rails db:migrate
