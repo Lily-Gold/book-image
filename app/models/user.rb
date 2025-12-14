4974755ec2757719609b185da950f1010c23e13c
Lily-Gold <lily.olive.yt@gmail.com>
2025-12-14 18:11:28 +0900

    add:Googleログインボタン

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: [:google_oauth2]
+         :omniauthable, omniauth_providers: [ :google_oauth2 ]
 
   has_one_attached :avatar
   has_many :books, dependent: :destroy
