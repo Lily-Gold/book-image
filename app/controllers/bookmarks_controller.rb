0000515587e41cc661e6add008f841adf5e272c2
Lily-Gold <lily.olive.yt@gmail.com>
2025-12-28 19:10:47 +0900

    add:ブックマーク機能（非同期）

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,26 @@
+class BookmarksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_review
+
+  def create
+    current_user.bookmarks.find_or_create_by!(review: @review)
+
+    respond_to do |format|
+      format.turbo_stream
+    end
+  end
+
+  def destroy
+    current_user.bookmarks.find_by!(review: @review).destroy!
+
+    respond_to do |format|
+      format.turbo_stream
+    end
+  end
+
+  private
+
+  def set_review
+    @review = Review.find_by!(public_id: params[:review_public_id])
+  end
+end
