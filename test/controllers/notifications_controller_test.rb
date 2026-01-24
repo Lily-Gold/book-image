ee1f3c3ab8d4c3b8817e61c9f9618ccdac464146
Lily-Gold <lily.olive.yt@gmail.com>
2026-01-25 01:30:02 +0900

    fix:githubテスト

--- test/controllers/notifications_controller_test.rb
+++ test/controllers/notifications_controller_test.rb
@@ -1,8 +1,13 @@
 require "test_helper"
 
 class NotificationsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+    sign_in @user
+  end
+
   test "should get index" do
-    get notifications_index_url
+    get notifications_url
     assert_response :success
   end
 end
