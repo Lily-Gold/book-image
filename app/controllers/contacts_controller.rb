997b6822a325d43fafb45979a8e44d2a1af8a05a
Lily-Gold <lily.olive.yt@gmail.com>
2026-02-08 20:14:10 +0900

    add:お問い合わせフォーム

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,27 @@
+class ContactsController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[new create thanks]
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+
+    if @contact.save
+      redirect_to thanks_contact_path
+    else
+      flash.now[:alert] = "お問い合わせの送信に失敗しました"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def thanks
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :content)
+  end
+end
