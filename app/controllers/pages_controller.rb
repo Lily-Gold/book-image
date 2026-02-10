19f63240f636e8a09e11862f99712882ef72f891
Lily-Gold <lily.olive.yt@gmail.com>
2026-02-11 01:30:28 +0900

    fix:リントチェック

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:terms, :privacy]
+  skip_before_action :authenticate_user!, only: [ :terms, :privacy ]
 
   def terms
   end
