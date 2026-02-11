2260d05b0b43b29e885fd9211e12cc9ab096b554
Lily-Gold <lily.olive.yt@gmail.com>
2026-02-11 17:13:52 +0900

    add:ハンバーガーメニューアニメーション

--- app/javascript/controllers/mobile_menu_controller.js
+++ app/javascript/controllers/mobile_menu_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["panel", "overlay"]
+  static targets = ["panel", "overlay", "line1", "line2", "line3", "icon"]
 
   toggle() {
     if (this.panelTarget.classList.contains("translate-x-full")) {
@@ -12,20 +12,26 @@ export default class extends Controller {
   }
 
   open() {
-    // メニュー表示
     this.panelTarget.classList.remove("translate-x-full")
 
-    // 背景表示
     this.overlayTarget.classList.remove("opacity-0", "pointer-events-none")
     this.overlayTarget.classList.add("opacity-100")
+
+    this.line1Target.classList.add("rotate-45", "translate-y-[6px]")
+    this.line2Target.classList.add("opacity-0")
+    this.line3Target.classList.add("-rotate-45", "-translate-y-[6px]")
+
   }
 
   close() {
-    // メニュー非表示
     this.panelTarget.classList.add("translate-x-full")
 
-    // 背景非表示
     this.overlayTarget.classList.add("opacity-0", "pointer-events-none")
     this.overlayTarget.classList.remove("opacity-100")
+
+    this.line1Target.classList.remove("rotate-45", "translate-y-[6px]")
+    this.line2Target.classList.remove("opacity-0")
+    this.line3Target.classList.remove("-rotate-45", "-translate-y-[6px]")
+
   }
 }
